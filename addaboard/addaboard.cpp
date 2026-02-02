0b96b85fad78110e8596d681279134a73f9b6843
Stein Gunnar Grastveit <grastvei@pvv.org>
2026-02-02 18:16:54 +0100

    ADDA board plugin

---
+++ addaboard/addaboard.cpp
@@ -0,0 +1,43 @@
+#include "addaboard.h"
+
+#include "3rdparty/ADS1256.h"
+#include "3rdparty/DEV_Config.h"
+
+#include <tagsystem/taglist.h>
+
+namespace plugin {
+
+bool AddaBoard::initialize()
+{
+	DEV_ModuleInit();
+
+	const QString subsystem("adda_board");
+
+	adc0Tag_ = tagList()->createTag(subsystem, "adc0", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc1Tag_ = tagList()->createTag(subsystem, "adc1", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc2Tag_ = tagList()->createTag(subsystem, "adc2", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc3Tag_ = tagList()->createTag(subsystem, "adc3", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc4Tag_ = tagList()->createTag(subsystem, "adc4", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc5Tag_ = tagList()->createTag(subsystem, "adc5", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc6Tag_ = tagList()->createTag(subsystem, "adc6", TagType::eDouble, 0.0, "Analog read [0-5]v");
+	adc7Tag_ = tagList()->createTag(subsystem, "adc7", TagType::eDouble, 0.0, "Analog read [0-5]v");
+
+	return true;
+}
+
+void AddaBoard::mainloop()
+{
+	UDOUBLE adc[8];
+	ADS1256_GetAll(adc);
+
+	adc0Tag_->setValue(adc[0] * 5.0 / 0x7fffff);
+	adc1Tag_->setValue(adc[1] * 5.0 / 0x7fffff);
+	adc2Tag_->setValue(adc[2] * 5.0 / 0x7fffff);
+	adc3Tag_->setValue(adc[3] * 5.0 / 0x7fffff);
+	adc4Tag_->setValue(adc[4] * 5.0 / 0x7fffff);
+	adc5Tag_->setValue(adc[5] * 5.0 / 0x7fffff);
+	adc6Tag_->setValue(adc[6] * 5.0 / 0x7fffff);
+	adc7Tag_->setValue(adc[7] * 5.0 / 0x7fffff);
+}
+
+} // namespace plugin
---
+++ addaboard/addaboard.h
@@ -0,0 +1,35 @@
+#ifndef ADDABOARD_H
+#define ADDABOARD_H
+
+#include <QObject>
+#include <plugins/plugincore/plugin.h>
+#include <tagsystem/tag.h>
+
+namespace plugin {
+
+class AddaBoard : public Plugin
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID "june.plugin.addaboard")
+  public:
+	AddaBoard() = default;
+
+	bool initialize() final;
+
+  private slots:
+	void mainloop() final;
+
+  private:
+	Tag *adc0Tag_ = nullptr;
+	Tag *adc1Tag_ = nullptr;
+	Tag *adc2Tag_ = nullptr;
+	Tag *adc3Tag_ = nullptr;
+	Tag *adc4Tag_ = nullptr;
+	Tag *adc5Tag_ = nullptr;
+	Tag *adc6Tag_ = nullptr;
+	Tag *adc7Tag_ = nullptr;
+};
+
+} // namespace plugin
+
+#endif
