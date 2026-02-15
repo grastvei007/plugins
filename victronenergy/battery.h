16002167b29f0b8fabaf553e3ae935a6183d2cfc
Stein Gunnar Grastveit <grastvei@pvv.org>
2026-02-15 08:36:36 +0100

    Move class Battery to its own file

---
+++ victronenergy/battery.h
@@ -0,0 +1,51 @@
+#ifndef BATTERY_H
+#define BATTERY_H
+
+#include <QObject>
+#include <tagsystem/tag.h>
+#include <tagsystem/taglist.h>
+#include <tagsystem/tagsocket.h>
+
+namespace plugin {
+
+class Battery : public QObject
+{
+	Q_OBJECT
+  public:
+	Battery(TagList *taglist,
+			const QString &subsystem,
+			const QString &name,
+			const QString &batteyName,
+			QObject *parent = nullptr);
+
+	void resetValues();
+
+	int chargedEnergy() const { return chargedEnergy_; }
+	int dischargedEnergy() const { return dischargedEnergy_; }
+
+  signals:
+	void chargedEnergyChanged();
+	void dischargedEnergyChanged();
+
+  private slots:
+	void onChargedEnergyChanged(double value);
+	void onDischargedEnergyChanged(double value);
+
+  private:
+	std::shared_ptr<Tag> chargedTodayTag_;
+	std::shared_ptr<Tag> dischargedTodayTag_;
+
+	std::shared_ptr<TagSocket> chargedEnergyTagSocket_;
+	std::shared_ptr<TagSocket> dischargedEnergyTagSocket_;
+
+	// total updated at midnight
+	int totalChargedEnergy_ = -1;
+	int totalDischargedEnergy_ = -1;
+	// dayly
+	int chargedEnergy_ = 0;
+	int dischargedEnergy_ = 0;
+};
+
+} // namespace plugin
+
+#endif // BATTERY_H
